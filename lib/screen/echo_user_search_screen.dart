9b4e4584b0d5b8891b8795a1029bc22c26363881
Siddhartha Basnet <siddbasnet2002@gmail.com>
2024-06-01 16:51:47 +0545

    User search screen updated

--- lib/screen/echo_user_search_screen.dart
+++ lib/screen/echo_user_search_screen.dart
@@ -1,12 +1,17 @@
+import 'package:echo1/component/verified_icon.dart';
+import 'package:echo1/providers/explore/explore_provider.dart';
+import 'package:echo1/utils/app_color.dart';
 import 'package:flutter/material.dart';
+import 'package:peaman_ui_components/peaman_ui_components.dart';
 
-class EchoUserSearchScreen extends StatefulWidget {
+class EchoUserSearchScreen extends ConsumerStatefulWidget {
   @override
   _EchoUserSearchScreenState createState() => _EchoUserSearchScreenState();
 }
 
-class _EchoUserSearchScreenState extends State<EchoUserSearchScreen> {
+class _EchoUserSearchScreenState extends ConsumerState<ConsumerStatefulWidget> {
   final FocusNode _focusNode = FocusNode();
+  final TextEditingController _searchController = TextEditingController();
 
   @override
   void initState() {
@@ -22,21 +27,128 @@ class _EchoUserSearchScreenState extends State<EchoUserSearchScreen> {
   void dispose() {
     _focusNode.dispose();
     super.dispose();
+    _searchController.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
+    final usersAsyncValue = ref.watch(providerOfUsers);
+    print(usersAsyncValue);
+
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Auto Focus TextField'),
+      appBar: _appBar(),
+      body: usersAsyncValue.when(
+        data: (users) {
+          final filteredUsers = users
+              .where(
+                (user) =>
+                    user.userName!.toLowerCase().contains(
+                          _searchController.text.toLowerCase().trim(),
+                        ) ||
+                    user.name!.toLowerCase().contains(
+                          _searchController.text.toLowerCase().trim(),
+                        ),
+              )
+              .toList();
+
+          return _searchController.text.trim().isEmpty
+              ? Container()
+              : filteredUsers.isEmpty
+                  ? const Center(
+                      child: Text("No user found"),
+                    )
+                  : ListView.builder(
+                      itemCount: filteredUsers.length,
+                      itemBuilder: (context, index) {
+                        return GestureDetector(
+                          onTap: () {
+                            Navigator.of(context).pushNamed(
+                              PeamanProfileScreen.route,
+                              arguments: PeamanProfileScreenArgs(
+                                userId: filteredUsers[index].uid!,
+                              ),
+                            );
+                          },
+                          child: _userTile(
+                            user: filteredUsers[index],
+                          ),
+                        );
+                      },
+                    );
+        },
+        error: (error, value) {
+          return PeamanErrorBuilder(
+            subTitle: value.toString(),
+          );
+        },
+        loading: () {
+          return const PeamanSpinner();
+        },
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: TextField(
-          focusNode: _focusNode,
-          decoration: InputDecoration(
-            labelText: 'Enter text here',
+    );
+  }
+
+  PreferredSizeWidget _appBar() {
+    return AppBar(
+      leading: Center(
+        child: PeamanRoundIconButton(
+          padding: EdgeInsets.all(7.w),
+          onPressed: context.pop,
+          icon: Icon(
+            Icons.arrow_back_rounded,
+            size: 16.w,
           ),
+        ),
+      ),
+      title: TextField(
+        controller: _searchController,
+        focusNode: _focusNode,
+        decoration: const InputDecoration(
+          hintText: "Search user",
+          prefixIcon: Icon(Icons.search),
+        ),
+        onChanged: (val) {
+          setState(() {});
+        },
+      ),
+    );
+  }
+
+  Widget _userTile({required PeamanUser user}) {
+    return ListTile(
+      leading: Stack(
+        alignment: Alignment.bottomRight,
+        children: [
+          user.photo != null
+              ? CircleAvatar(
+                  radius: 25.0,
+                  backgroundImage: NetworkImage(
+                    user.photo!,
+                  ),
+                )
+              : const CircleAvatar(
+                  radius: 25.0,
+                  backgroundImage: AssetImage(
+                    'assets/images/avatar_unknown.png',
+                    package: 'peaman_ui_components',
+                  ),
+                ),
+          user.isVerified ? const VerifiedIcon() : const SizedBox(),
+        ],
+      ),
+      title: Text(
+        user.userName!,
+        style: const TextStyle(
+          color: AppColor.black,
+          fontSize: 16.0,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+      subtitle: Text(
+        user.name!,
+        style: const TextStyle(
+          color: AppColor.black,
+          fontSize: 14.0,
         ),
       ),
     );
